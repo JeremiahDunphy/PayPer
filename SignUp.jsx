f7c6259ffdc692ec5eda5c3d5be1b66a30751569
Jeremiah <miahedunphy@gmail.com>
2023-09-14 01:49:07 -0400

    added skel layout for signup page

--- SignUp.jsx
+++ SignUp.jsx
@@ -1,9 +1,43 @@
+import { Text, TextInput, View, Button } from "react-native";
+import { SignUpStyles } from "./SignUpStyles";
+import { useState } from "react";
 
-import {
-  Text,
-} from "react-native";
 export const SignUp = () => {
-    return (
-        <Text>Welcome to the Sugn up Page.</Text>
-    )
-}
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    confirmEmail: "",
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const inputFields = [
+    { name: "firstName", placeholder: "Please Enter Your First Name" },
+    { name: "lastName", placeholder: "Please Enter Your Last Name" },
+    { name: "email", placeholder: "Please Enter Your Email" },
+    { name: "confirmEmail", placeholder: "Please Confirm Your Email" },
+    { name: "username", placeholder: "Please Create Your Username" },
+    { name: "password", placeholder: "Please Create Your Password" },
+  ];
+
+  return (
+    <View style={SignUpStyles.container}>
+      {inputFields.map((field, index) => (
+        <TextInput
+          key={index}
+          style={SignUpStyles.input}
+          placeholder={field.placeholder}
+          onChangeText={(value) => handleChange(field.name, value)}
+        />
+      ))}
+      <Button title="Sign Up"></Button>
+    </View>
+  );
+};
---
+++ SignUpStyles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native"
+
+export const SignUpStyles = {
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 10,
+    fontSize: 16,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#f0f0f0",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+};
